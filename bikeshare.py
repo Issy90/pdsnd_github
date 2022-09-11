228bdc26c00c4d41b0a904256219a7af93b4a6fb
Isra Khan <issy.ib@gmail.com>
2022-09-12 01:44:02 +0300

    Added comments on bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -2,6 +2,7 @@ import time
 import pandas as pd
 import numpy as np
 
+# city data which includes chicago, new york and washington
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
